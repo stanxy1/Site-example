1b20337336c6c09ed7be5fd5040219384113a193
stanxy1 <153308152+stanxy1@users.noreply.github.com>
2023-12-20 00:09:44 +0000

    Minor improvements

--- second-one/app2.js
+++ second-one/app2.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 var dives = [$('.first_line'), $('.second_line'), $('.third_line'), $('.fourth_line'), $('.fifth_line'), $('.sixth_line'), $('.seventh_line')];
-var colors = ['red', 'orange', 'yellow', 'green', 'lightblue', 'blue', 'purple'];
+var colors = ['red', 'orange', 'yellow', 'green', 'aqua', 'blue', 'purple'];
 var currentIndex = 0;
 setInterval(function () {
   dives[Math.floor(Math.random()*7)].css({
