590a4671f70fb5ab8e06db2ca70a026165f2eaa4
stanxy1 <153308152+stanxy1@users.noreply.github.com>
2023-12-21 16:46:34 +0000

    Not app2js, needs minor fixes.... STYLE UNDEFINED????

--- second-one/app2.js
+++ second-one/app2.js
@@ -1,13 +1,15 @@
 window.onload = function(){
-let dives = [document.getElementById(1), document.getElementById(2), document.getElementById(3), document.getElementById(4), document.getElementById(5), document.getElementById(6), document.getElementById(7)];
+
+let dives = [document.getElementById(1), document.getElementById(2), document.getElementById(3), document.getElementById(4)];
 let colors = ['red', 'orange', 'yellow', 'green', 'aqua', 'blue', 'purple'];
 let currentIndex = 0;
-setInterval(function () {
-  dives[currentIndex].style.backgroundColor = colors[currentIndex]
+//setInterval(function () {
+  dives[Math.floor(Math.random()*7)].style.backgroundColor = colors[currentIndex];
+  
    if (!colors[currentIndex]) {
        currentIndex = 0;
    } else {
        currentIndex++;
    }
-}, 50);
+//}, 50);
 }
