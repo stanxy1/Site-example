189e5a363d8e650db89b1a6e25336c2ce326dc98
Primis1 <laleyla3361@gmail.com>
2023-12-23 20:03:02 -0500

    almost everything works perfectly, but need few fixes in the future

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// object.onclick = function(){MyFunction}
-
-// function MyFunction(    ){
-//     alert("SERKAN GAY");
-// }
-
 let lastScrollTop = 0;
 header = document.getElementById('header');
 window.addEventListener('scroll', function() {
@@ -16,13 +10,6 @@ window.addEventListener('scroll', function() {
   lastScrollTop = scrollTop;
 });
 
-
-
-function replace() {
-  window.location.href = "second-one/index2.html";
-}
-
-
 let btnTop = document.getElementById('btnTop');
 
 window.onscroll = function() {
@@ -30,10 +17,10 @@ window.onscroll = function() {
 };
 
 function topFuncrion() {
-  if ( document.documentElement.scrollTop > 120) {
-    btnTop.style.size = "1";
+  if ( document.documentElement.scrollTop > 120 && document.documentElement.scrollTop < 1275) {
+    btnTop.style.transform = "scale(1)";
   } else {
-    btnTop.style.size = "0";
+    btnTop.style.transform = "scale(0)";
   }
 }
 
@@ -41,3 +28,52 @@ function toUp() {
   window.scrollTo({top: 0, behavior: 'smooth'});
   // document.documentElement.scrollTop = 0;
 }
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+let animationActive = false;
+
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    circle.style.opacity = "1";
+
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  if(animationActive) {
+    requestAnimationFrame(animateCircles);
+  }
+}
+
+document.querySelector('.btn2').addEventListener('click', function() {
+  animationActive = !animationActive;
+  if(animationActive) {
+    animateCircles();
+  }
+});
